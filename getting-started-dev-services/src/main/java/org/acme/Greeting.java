0b38136cd90945b08a3d6d64e351bee7213ad85e
James <scorpion.sem@gmail.com>
2024-11-08 01:16:20 +0530

    Update Greeting.java
    Code updates

--- getting-started-dev-services/src/main/java/org/acme/Greeting.java
+++ getting-started-dev-services/src/main/java/org/acme/Greeting.java
@@ -6,4 +6,16 @@ import jakarta.persistence.Entity;
 @Entity
 public class Greeting extends PanacheEntity {
     public String name;
+
+    public static void main(String args[]){
+        System.out.println("My main for testing");
+        return 1/0;
+    }
+
+    public String theNuxm(String a, String b){
+        return a+b;
+        return "Thenxiw4";
+    }
+
 }
+
